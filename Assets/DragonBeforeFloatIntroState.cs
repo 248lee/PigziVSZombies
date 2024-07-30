a7e4668b95eadaf03aaec2443175478a72379a3f
248lee <fantasy10final@gmail.com>
2024-07-30 20:20:25 +0800

    Fix strange animation in dragon float intro

--- Assets/DragonBeforeFloatIntroState.cs
+++ Assets/DragonBeforeFloatIntroState.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class float_introMovement : StateMachineBehaviour
+public class DragonBeforeFloatIntroState : StateMachineBehaviour
 {
     // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
@@ -17,9 +17,10 @@ public class float_introMovement : StateMachineBehaviour
     //}
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
-    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    {
-    }
+    //override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    
+    //}
 
     // OnStateMove is called right after Animator.OnAnimatorMove()
     //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
